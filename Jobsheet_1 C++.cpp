44b60a79995f4c4707ea0d7fe2321e9a3b41390a
Chaeru Rachmadi Putra <64574882+Chaeru7@users.noreply.github.com>
2020-05-17 00:20:23 +0800

    Add files via upload
    Menampilkan array satu dimensi dengan bahasa C++

---
+++ Jobsheet_1 C++.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int my_array[6] = { 5,6,9,8,10,11 };
+	cout<<"NIM 6"<<endl<<endl;
+	cout<<"Data ke - 1 = "<<my_array[0]<<endl;
+	cout<<"Data ke - 2 = "<<my_array[1]<<endl;
+	cout<<"Data ke - 3 = "<<my_array[2]<<endl;
+	cout<<"Data ke - 4 = "<<my_array[3]<<endl;
+	cout<<"Data ke - 5 = "<<my_array[4]<<endl;	
+	cout<<"Data ke - 6 = "<<my_array[5]<<endl;
+}
