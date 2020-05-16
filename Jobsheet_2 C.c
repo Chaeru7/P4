1e7402d9b1df479fd339d17c503357c3089ca2b2
Chaeru Rachmadi Putra <64574882+Chaeru7@users.noreply.github.com>
2020-05-17 00:27:22 +0800

    Add files via upload
    Menampilkan array dua dimensi

---
+++ Jobsheet_2 C.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+main()
+{
+int x [6];
+printf("Masukkan nilai ke -1 : ");
+scanf("%i",&x[0]);
+printf("Masukkan nilai ke -2 : ");
+scanf("%i",&x[1]);
+printf("Masukkan nilai ke -3 : ");
+scanf("%i",&x[2]);
+printf("Masukkan nilai ke -4 : ");
+scanf("%i",&x[3]);
+printf("Masukkan nilai ke -5 : ");
+scanf("%i",&x[4]);
+printf("Masukkan nilai ke -5 : ");
+scanf("%i",&x[5]);
+printf("\n\n");
+printf("nim ke-1 =%i\n",x[0]);
+printf("nim ke-2 =%i\n",x[1]);
+printf("nim ke-3 =%i\n",x[2]);
+printf("nim ke-3 =%i\n",x[3]);
+printf("nim ke-3 =%i\n",x[4]);
+printf("nim ke-3 =%i\n",x[5]);
+return 0;
+}
+
